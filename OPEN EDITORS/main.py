02c3579240d8f50cddb0a15dfcefca611b184a5c
melissapennington <mlpennington9@gmail.com>
2024-09-20 11:51:50 -0500

    start of summary

--- OPEN EDITORS/main.py
+++ OPEN EDITORS/main.py
@@ -6,6 +6,7 @@ from data_entry import get_amount, get_category, get_date, get_description
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -44,12 +45,41 @@ class CSV:
                 print("Entry added successfully")
         except Exception as e:
             print(f"An error occurred while writing to the file: {e}")
+    
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.FORMAT)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+        end_date = datetime.strptime(end_date, CSV.FORMAT)
 
-# if __name__ == "__main__":
-#     CSV.initialize_csv()
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
 
-#     # Call add_entry with the correct class name
-#     CSV.add_entry("20-07-2024", 125, "Income", "Salary")
+        if filtered_df.empty:
+            print("No transactions found in the given date range")
+        else:
+            print(
+                f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}"
+            )
+            print(
+                filtered_df.to_string(
+                    index=False, formatters={"date": lambda x: x.strftime(CSV.FORMAT)}
+                )
+            )
+
+            total_income = filtered_df[filtered_df["category"] == "Income"][
+                "amount"
+            ].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"][
+                "amount"
+            ].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income  - total_expense):.2f}")
+
+        return filtered_df
 
 def add():
     CSV.initialize_csv()
@@ -62,4 +92,4 @@ def add():
     description = get_description()
     CSV.add_entry(date, amount, category, description)
 
-add()
+CSV.get_transactions("01-01-2024", "12-12-2024")
